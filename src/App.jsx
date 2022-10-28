2e844a01ba8b6627a5a55ca6e7ad5807bbd784ee
Tsilos Kosbab <38735446+Tsilos1@users.noreply.github.com>
2022-10-28 13:07:23 -0500

    added style buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import StylesContext from './StylesContext'
+import UserInterface from './UserInterface';
 import './App.css';
 
 import UserInterface from "./UserInterface"
 
 function App() {
+  const stylin = {
+  background: "#1ECD97",
+    display: "inline-block", 
+    width: 120,
+    height: 55,
+    fontSize: 18,
+    letterSpacing: 1,
+    border: "2px solid #1ECD97",
+    borderRadius: 15
+     };
+  
+  
   return (
     <main>
+    <StylesContext.Provider value={stylin}>
      <h1
           style={{
             color: "#FEE001",
@@ -14,10 +29,12 @@ function App() {
           }}
         >
           Welcome to Comics Galore!
-        </h1>
-        <UserInterface />
+      </h1>
+      <UserInterface />
+      </StylesContext.Provider>
     </main>
   );
-}
+}             
+
 
 export default App;
